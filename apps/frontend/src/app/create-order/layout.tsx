b10b5e6352134ac436cc486b495bf0131090ba90
brucecheng492 <brucecheng492@gmail.com>
2025-07-10 01:45:50 +0800

    fix(ui): attempt #2

--- apps/frontend/src/app/create-order/layout.tsx
+++
@@ -1,9 +0,0 @@
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Create Order Form",
-};
-
-export default function AdminLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-}
